4de07bd41545282f63611e9a2c4fb2a3bfa41b90
Shadik Hossain <mdshadikhossain830@gmail.com>
2025-03-24 16:35:58 +0600

    Updated code

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+res.setHeader("Content-Type", "application/pdf");
+
 // Redirect "/" to "/home"
 app.get("/", (req, res) => {
   res.redirect("/home");
