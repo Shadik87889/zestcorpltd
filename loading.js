670dc306a7a394f3e0fd20cc192590e76c4cfb7a
Shadik Hossain <mdshadikhossain830@gmail.com>
2025-03-26 15:22:42 +0600

    Updated code

--- loading.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function () {
-  // Hide the loading screen once everything is fully loaded
-  const loadingScreen = document.getElementById("loading-screen");
-  loadingScreen.style.display = "none";
-});
